9416a30f80d61281600b23207bddab78db75fbae
Thiago Rocha <thiagorocha503@gmail.com>
2019-12-05 19:18:36 -0300

    Atualiza urls

--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5
     path('<int:question_id>/', views.detail, name='detail'),
