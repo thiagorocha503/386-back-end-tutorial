0bdfe2ae51d6ef6975d1a41a693124e494e9984f
Thiago Rocha <thiagorocha503@gmail.com>
2019-12-05 19:20:57 -0300

    :art:PEP8

--- polls/views.py
+++ polls/views.py
@@ -8,19 +8,22 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    context =  {'latest_question_list': latest_question_list,}
-    return render(request,'polls/index.html', context)
-    #return HttpResponse(template.render(context, request))
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list, }
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
-    #return HttpResponse("You're looking at question %s." % question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     response = "You're looking at the results of questions.%s"
     return HttpResponse(response % question_id)
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
